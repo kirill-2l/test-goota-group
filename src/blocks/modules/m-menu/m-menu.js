dd0b7c0484478a383a4b87423d3f8ad5e9863dc7
Kirill <petrovvkirill@gmail.com>
2020-08-01 18:07:26 +0300

    update gh-pages

--- src/blocks/modules/m-menu/m-menu.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const menuContainer = document.querySelector(".m-menu-container");
-
-hamburger.addEventListener("click", function () {
-    menuContainer.classList.toggle("m-menu-container--active");
-});
--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import "./import/modules";
-import "./import/components";
-
-// import "../blocks/modules/form/form";
-// import "%modules%/form/form";
